d54a2062dc9b6396b09979f5bdf9dd343cbaf94c
Lukas Prost <prost@vrvis.at>
2017-07-04 11:41:14 +0200

    rendering working

---
+++ src/RTPAL/RenderState.fs
@@ -0,0 +1,21 @@
+﻿namespace Render
+
+open Aardvark.Base.Incremental
+open Aardvark.Base
+open Aardvark.UI
+open Aardvark.UI.Primitives
+open Aardvark.Base.ShaderReflection.ShaderPath
+
+type Action =
+    | IMPORT
+    | CAMERA of CameraController.Message
+
+[<DomainType>]
+type RenderState =
+    {    
+        files : list<string>
+        scenes : hset<ISg<Action>>
+        bounds : Box3d
+        cameraState : CameraControllerState
+    }
+
