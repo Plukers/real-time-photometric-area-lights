fd818600a655bfd9164ea3bee1bb3745718fde6b
Lukas Prost <prost@vrvis.at>
2017-11-22 16:08:36 +0100

    some further development

--- src/RTPAL/EffectApPoint.fs
+++ src/RTPAL/EffectApPoint.fs
@@ -79,36 +79,80 @@ module EffectApPoint =
         }
 
     [<ReflectedDefinition>]       
-    let clampMRPToTriangle (a : V3d) (b : V3d) (c : V3d) (mrp : V3d) (t2l : M33d) = 
+    let clampPointToTriangle (a : V3d) (b : V3d) (c : V3d) (p : V3d) (tts : M33d) = // tls = transformation to triangle space matrix
         let a2d = 
-            let v = t2l * (a - a)
+            let v = tts * (a - a)
             V2d(v.X, v.Y)
 
         let b2d = 
-            let v = t2l * (b - a)
+            let v = tts * (b - a)
             V2d(v.X, v.Y)
 
         let c2d = 
-            let v = t2l * (c - a)
+            let v = tts * (c - a)
             V2d(v.X, v.Y)
 
-        let m2d = 
-            let v = t2l * (mrp - a)
+        let p2d = 
+            let v = tts * (p - a)
             V2d(v.X, v.Y)
           
-        let clampedMrp = 
-            match barycentricCoordinates m2d a2d b2d c2d with
-            | (u, v, w) when u <= 0.0 && v <= 0.0 && w >  0.0 -> c                          //   I
-            | (u, v, w) when u >  0.0 && v <  0.0 && w >  0.0 -> projectPointOnLine c a mrp //  II
-            | (u, v, w) when u >  0.0 && v <= 0.0 && w <= 0.0 -> a                          // III
-            | (u, v, w) when u >  0.0 && v >  0.0 && w <  0.0 -> projectPointOnLine a b mrp //  IV
-            | (u, v, w) when u <= 0.0 && v >  0.0 && w <= 0.0 -> b                          //   V
-            | (u, v, w) when u <  0.0 && v >  0.0 && w >  0.0 -> projectPointOnLine b c mrp //  VI
-            | _ -> mrp // all coordinates are positive
+        let clampedP = 
+            match barycentricCoordinates p2d a2d b2d c2d with
+            | (u, v, w) when u <= 0.0 && v <= 0.0 && w >  0.0 -> c                        //   I
+            | (u, v, w) when u >  0.0 && v <  0.0 && w >  0.0 -> projectPointOnLine c a p //  II
+            | (u, v, w) when u >  0.0 && v <= 0.0 && w <= 0.0 -> a                        // III
+            | (u, v, w) when u >  0.0 && v >  0.0 && w <  0.0 -> projectPointOnLine a b p //  IV
+            | (u, v, w) when u <= 0.0 && v >  0.0 && w <= 0.0 -> b                        //   V
+            | (u, v, w) when u <  0.0 && v >  0.0 && w >  0.0 -> projectPointOnLine b c p //  VI
+            | _ -> p // all coordinates are positive
 
-        clampedMrp
+        clampedP
+
+    [<ReflectedDefinition>] 
+    let linePointDistance (l1 : V2d) (l2 : V2d) (pnt : V2d) =
+            let dir = l2 - l1 |> Vec.normalize
+            let n = V2d(dir.Y, -dir.X)
+            pnt - l1 |> Vec.dot n
+
+    [<ReflectedDefinition>] 
+    let clampPointToPolygon (a : V3d) (b : V3d) (c : V3d) (d : V3d) (p : V3d) (tps : M33d) = // tls = transformation to polygon space matrix
+        let a2d = 
+            let v = tps * (a - a)
+            V2d(v.X, v.Y)
+
+        let b2d = 
+            let v = tps * (b - a)
+            V2d(v.X, v.Y)
+
+        let c2d = 
+            let v = tps * (c - a)
+            V2d(v.X, v.Y)
+
+        let d2d = 
+            let v = tps * (d - a)
+            V2d(v.X, v.Y)
+
+        let p2d = 
+            let v = tps * (p - a)
+            V2d(v.X, v.Y)
+
+        let t = linePointDistance a2d b2d p2d
+        let u = linePointDistance b2d c2d p2d
+        let v = linePointDistance c2d d2d p2d
+        let w = linePointDistance d2d a2d p2d
         
-
+        match (t, u, v, w) with
+        | (t, u, v, w) when t >  0.0 && u <= 0.0 && v <= 0.0 && w <= 0.0 -> projectPointOnLine a b p //    I
+        | (t, u, v, w) when t >  0.0 && u >  0.0 && v <= 0.0 && w <= 0.0 -> b                        //   II
+        | (t, u, v, w) when t <= 0.0 && u >  0.0 && v <= 0.0 && w <= 0.0 -> projectPointOnLine b c p //  III
+        | (t, u, v, w) when t <= 0.0 && u >  0.0 && v >  0.0 && w <= 0.0 -> c                        //   IV
+        | (t, u, v, w) when t <= 0.0 && u <= 0.0 && v >  0.0 && w <= 0.0 -> projectPointOnLine c d p //    V
+        | (t, u, v, w) when t <= 0.0 && u <= 0.0 && v >  0.0 && w >  0.0 -> d                        //   VI
+        | (t, u, v, w) when t <= 0.0 && u <= 0.0 && v <= 0.0 && w >  0.0 -> projectPointOnLine d a p //  VII
+        | (t, u, v, w) when t >  0.0 && u <= 0.0 && v <= 0.0 && w >  0.0 -> a                        // VIII
+        | _ -> p // all coordinates are positive
+        
+        
     
     let mostRepresentativePointApprox (v : Vertex) = 
         fragment {
@@ -173,24 +217,21 @@ module EffectApPoint =
                                 if (Vec.dot closestPoint V3d.OOI) < 0.0 then
                                     let newDir = V3d(closestPoint.X, closestPoint.Y, 0.0) |> Vec.normalize
                                     closestPoint <- linePlaneIntersection V3d.Zero newDir (clippedVa.[0]) lightPlaneN
-
-                                let abovePlane = if (Vec.dot closestPoint lightPlaneN) > 0.0 then false else true
-
+                                    
                                 let insideLightPlane = (Vec.length closestPoint) < eps
-
-
+                                
                                 if not insideLightPlane then
                                     
                                     let closestPointDir = closestPoint |> Vec.normalize
 
                                     // intersect normal with plane
-
-                                    (*
                                     let mutable up = V3d.OOI
                                 
                                     if abs(Vec.dot up lightPlaneN) < eps then
                                         up <- up + (epb * closestPointDir) |> Vec.normalize     
                                     else
+                                        
+                                        let abovePlane = if (Vec.dot V3d.OOI closestPoint) < 0.0 && (Vec.dot closestPoint lightPlaneN) < 0.0 then false else true
                                         if abovePlane then
                                             let lpn = 
                                                 if (Vec.dot up lightPlaneN) > 0.0 then
@@ -199,8 +240,8 @@ module EffectApPoint =
                                                     -lightPlaneN
 
                                             up <- up + (abs(Vec.dot up lpn) + epb) * (-lpn) |> Vec.normalize
-                                    *)
                                     
+                                    (*
                                     let mutable up = clippedVa.[0]
                                     let mutable avgc = 1.0
                                     for l in 1 .. clippedVc - 1 do
@@ -214,25 +255,26 @@ module EffectApPoint =
                                         else
                                             if zdiff < 0.0 then
                                                 up <- clippedVa.[l]
+                                    *)
                                     
-
-                                    let normPlanePointDir = up // linePlaneIntersection V3d.Zero up (clippedVa.[0]) lightPlaneN |> Vec.normalize
+                                    let normPlaneP = linePlaneIntersection V3d.Zero up (clippedVa.[0]) lightPlaneN // tangent space
 
                                     // find most representative point
 
-                                    let mrpDir = closestPointDir + normPlanePointDir |> Vec.normalize
+                                    // let mrpDir = closestPointDir + normPlanePointDir |> Vec.normalize
                                     // let mrpDir = closestPointDir //TODO remove
 
-                                    let mrp = linePlaneIntersection V3d.Zero mrpDir (clippedVa.[0]) lightPlaneN // tangent space
+                                    // let mrp = linePlaneIntersection V3d.Zero mrpDir (clippedVa.[0]) lightPlaneN // tangent space
                               
-                                    let (mrp, sa) = 
+                                    let (closestPointDir, normPlanePDir, sa) = 
 
                                         if clippedVc = 3 then
                                             let sa = computeSolidAngle clippedVa.[0] clippedVa.[1] clippedVa.[2]
 
-                                            let mrp = clampMRPToTriangle clippedVa.[0] clippedVa.[1] clippedVa.[2] mrp t2l   
-                                        
-                                            (mrp, sa)
+                                            let closestPoint = clampPointToTriangle clippedVa.[0] clippedVa.[1] clippedVa.[2] closestPoint t2l   
+                                            let normPlaneP =   clampPointToTriangle clippedVa.[0] clippedVa.[1] clippedVa.[2] normPlaneP t2l 
+
+                                            (closestPoint |> Vec.normalize, normPlaneP |> Vec.normalize, sa)
 
                                         else
 
@@ -240,27 +282,16 @@ module EffectApPoint =
                                             let sa2 = computeSolidAngle clippedVa.[0] clippedVa.[2] clippedVa.[3]
                                             let sa = sa1 + sa2
 
-                                            let mutable barycenterA = V3d.Zero
-                                            barycenterA <- barycenterA + clippedVa.[0] / 3.0
-                                            barycenterA <- barycenterA + clippedVa.[1] / 3.0
-                                            barycenterA <- barycenterA + clippedVa.[2] / 3.0
+                                            let closestPoint = clampPointToPolygon clippedVa.[0] clippedVa.[1] clippedVa.[2] clippedVa.[3] closestPoint t2l   
+                                            let normPlaneP =   clampPointToPolygon clippedVa.[0] clippedVa.[1] clippedVa.[2] clippedVa.[3] normPlaneP t2l  
+     
+                                            (closestPoint |> Vec.normalize, normPlaneP |> Vec.normalize, sa)
 
-                                            let mutable barycenterB = V3d.Zero
-                                            barycenterB <- barycenterB + clippedVa.[0] / 3.0
-                                            barycenterB <- barycenterB + clippedVa.[2] / 3.0
-                                            barycenterB <- barycenterB + clippedVa.[3] / 3.0       
-
-                                            let mrp = 
-                                                if ((mrp - barycenterA) |> Vec.length) > ((mrp - barycenterB) |> Vec.length) then
-                                                    clampMRPToTriangle clippedVa.[0] clippedVa.[1] clippedVa.[2] mrp t2l
-                                                else
-                                                    clampMRPToTriangle clippedVa.[0] clippedVa.[2] clippedVa.[3] mrp t2l
-
-                                            (mrp, sa)
+                                    let mrpDir  = closestPointDir + normPlanePDir |> Vec.normalize
                                             
 
                                             
-                                    let i =  mrp
+                                    let i =  mrpDir
                                     let d = Vec.length i
                                     let i = i |> Vec.normalize
                                 
